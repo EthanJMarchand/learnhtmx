138f8837ab9afc1cf59dbc4db03af2b0e29e9781
Matthew Boyle <hello@mattjamesboyle.com>
2024-04-06 17:52:58 +0100

    add tests to show funciton

--- internal/controllers/contacts_test.go
+++ internal/controllers/contacts_test.go
@@ -1,56 +1,147 @@
-package controllers
+package controllers_test
 
 import (
 	"database/sql"
+	"github.com/ethanjmarchand/learnhtmx/internal/controllers"
+	"github.com/ethanjmarchand/learnhtmx/internal/errors"
 	"github.com/ethanjmarchand/learnhtmx/internal/models"
+	"github.com/ethanjmarchand/learnhtmx/internal/models/mocks"
+	"github.com/golang/mock/gomock"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
-type Tester struct {
-	// here, we can add a few extra data fields to increment when the function runs.
-	Calls int
-	DB    *sql.DB
-}
+func TestContacts_Show(t *testing.T) {
+	t.Run("gives an internal server error given no contacts in DB and no query string", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
 
-func NewTesterContactRepo(db *sql.DB) *Tester {
-	return &Tester{
-		DB: db,
-	}
-}
+		mockTemplate := mocks.NewMockTemplate(ctrl)
 
-func (t Tester) Read(name string) (*[]models.Contact, error) {
-	return nil, nil
-}
+		mr := mocks.NewMockContactRepo(ctrl)
+		c := models.NewContactService(mr)
+		mctrl := controllers.Contacts{
+			Templates: struct {
+				Contacts controllers.Template
+				New      controllers.Template
+				View     controllers.Template
+				Edit     controllers.Template
+			}{
+				Contacts: mockTemplate,
+			},
+			ContactService: c,
+		}
 
-func (t Tester) ReadAll() (*[]models.Contact, error) {
-	return nil, nil
-}
+		mr.EXPECT().ReadAll().Times(1).Return(nil, sql.ErrNoRows)
+		// This validates the template renders with the error
+		mockTemplate.EXPECT().
+			Execute(gomock.Any(), gomock.Any(), gomock.Any(), errors.Public(sql.ErrNoRows, "No results")).
+			AnyTimes()
 
-func (t Tester) GetContact(id int) (*models.Contact, error) {
-	t.Calls++
-	return nil, nil
-}
+		req, err := http.NewRequest("GET", "/contacts", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-func (t Tester) New(contact *models.Contact) error {
-	return nil
-}
-func (t Tester) Update(contact *models.Contact) error {
-	return nil
-}
+		// Create a ResponseRecorder to record the response
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(mctrl.Show)
 
-func (t Tester) Delete(contact *models.Contact) error {
-	return nil
-}
+		// Perform the request
+		handler.ServeHTTP(rr, req)
 
-func TestContacts_Edit(t *testing.T) {
-	testCR := NewTesterContactRepo(&sql.DB{})
-	testCS := models.NewContactService(testCR)
-	contactTester := Contacts{
-		ContactService: testCS,
-	}
-	w := httptest.NewRecorder()
-	r := http.Request{}
-	contactTester.Edit(w, &r)
+	})
+
+	t.Run("gives an internal server error given no contacts matching query string", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mockTemplate := mocks.NewMockTemplate(ctrl)
+
+		mr := mocks.NewMockContactRepo(ctrl)
+		c := models.NewContactService(mr)
+		mctrl := controllers.Contacts{
+			Templates: struct {
+				Contacts controllers.Template
+				New      controllers.Template
+				View     controllers.Template
+				Edit     controllers.Template
+			}{
+				Contacts: mockTemplate,
+			},
+			ContactService: c,
+		}
+
+		req, err := http.NewRequest("GET", "/contacts", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		form := url.Values{}
+		form.Add("q", "John Doe")
+		req.URL.RawQuery = form.Encode()
+
+		// Create a ResponseRecorder to record the response
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(mctrl.Show)
+
+		mr.EXPECT().Read("John Doe").Times(1).Return(nil, sql.ErrNoRows)
+		// This validates the template renders with the error
+		mockTemplate.EXPECT().
+			Execute(gomock.Any(), gomock.Any(), gomock.Any(), errors.Public(sql.ErrNoRows, "No results")).
+			AnyTimes()
+
+		// Perform the request
+		handler.ServeHTTP(rr, req)
+
+	})
+
+	t.Run("gives no error given the user can be found in the DB", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mockTemplate := mocks.NewMockTemplate(ctrl)
+
+		mr := mocks.NewMockContactRepo(ctrl)
+		c := models.NewContactService(mr)
+		mctrl := controllers.Contacts{
+			Templates: struct {
+				Contacts controllers.Template
+				New      controllers.Template
+				View     controllers.Template
+				Edit     controllers.Template
+			}{
+				Contacts: mockTemplate,
+			},
+			ContactService: c,
+		}
+
+		expectedContacts := []models.Contact{{Name: "John Doe", Email: "johndoe@example.com"}}
+
+		req, err := http.NewRequest("GET", "/contacts", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		form := url.Values{}
+		form.Add("q", "John Doe")
+		req.URL.RawQuery = form.Encode()
+
+		// Create a ResponseRecorder to record the response
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(mctrl.Show)
+
+		mr.EXPECT().Read("John Doe").Times(1).Return(&expectedContacts, nil)
+		// This validates the template renders with the error
+		mockTemplate.EXPECT().
+			Execute(gomock.Any(), gomock.Any(), gomock.Any()).
+			AnyTimes()
+
+		// Perform the request
+		handler.ServeHTTP(rr, req)
+
+	})
+
 }
